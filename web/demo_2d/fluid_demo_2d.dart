78a9f906ec9e13b4b1ceb5615a247f8f5d0d1a7c
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2013-11-21 22:42:14 +0100

    added simple FPS display

--- web/demo_2d/fluid_demo_2d.dart
+++ web/demo_2d/fluid_demo_2d.dart
@@ -5,24 +5,31 @@ import 'package:fluid_mvm/fluid_mvm_renderer2d.dart';
 FluidSimulator simulator;
 CanvasRenderingContext2D context;
 
+const _deltaCount = 20;
+const _dividend = _deltaCount * 1000;
+List<double> deltas = new List.generate(_deltaCount, (_) => 0.0, growable: false);
+int frame = 0;
+double lastTime = 0.0;
+
 void main() {
   CanvasElement canvas = querySelector("#fluid_canvas");
   context = canvas.context2D;
-  
+  context.textBaseline = 'top';
+
   // Create the fluid simulator
   simulator = new FluidSimulator();
   simulator.initializeGrid(canvas.width, canvas.height);
-  
+
   // Add some initial particles
   for (int i = 0; i < 50; i++) {
     for (int j = 0; j < 50; j++) {
       simulator.addParticle(i * 2 + 10, j * 2 + 10, 0.5, 0);
     }
   }
-  
+
   // Create a 2d renderer to draw the fluid
   simulator.renderer = new FluidRenderer2D(context);
-  
+
   window.animationFrame.then(draw);
 }
 
@@ -34,9 +41,17 @@ void draw(num elapsedTime) {
 
   // Update the simulation
   simulator.update();
-  
+
   // Draw the fluid particles
   simulator.draw();
-  
+
+  deltas[frame++ % _deltaCount] = elapsedTime - lastTime;
+  lastTime = elapsedTime;
+
+  var fps = _dividend / deltas.reduce((combined, current) => combined + current);
+
+  context.fillStyle = 'black';
+  context.fillText('FPS: ${fps.toStringAsFixed(2)}', 5, 5);
+
   window.animationFrame.then(draw);
 }
